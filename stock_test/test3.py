b87c5dde7e219e6a7b0906fafb5c5b54b907fd5d
Unknown <specialist.yoon@gmail.com>
2017-07-18 12:35:17 +0900

    2017.07.17
    스터디 후

--- stock_test/test3.py
+++ stock_test/test3.py
@@ -3,10 +3,10 @@ import os, sys, time, csv, pprint, sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
 
-con = sqlite3.connect('002000.db')
+con = sqlite3.connect('023960.db')
 cur = con.cursor()
 FROM = '1984-01-01'
-TO = '2017-01-31'
+TO = '2017-07-31'
 
 FROM_unixtime = time.mktime(time.strptime(FROM, "%Y-%m-%d"))
 TO_unixtime = time.mktime(time.strptime(TO, "%Y-%m-%d"))
@@ -47,8 +47,6 @@ for index in range(len(DataSet[:,4])):
         long_values.append(sv)
 # print long_values
 
-
-
 # 신고가:[일]0봉전 고가가 10봉중 신고가
 # [일]거래량:100000이상 999999999이하
 # 주가등락률:[일]1봉전(중) 종가대비 0봉전 종가등락률 1%이상
@@ -57,36 +55,30 @@ for index in range(len(DataSet[:,4])):
 # 이평이격도:[일]0봉전(종가 60, 종가 120) 3%이내 근접 1회이상
 #n일 이격도 = (현재 주가 / n일 이동평균) x 100
 
-
 #open1 , high2 , low3, close4, volume5
 position=[] # None , 1:buy, 2:Sell, 3: Buy&Sell
 for index, item in enumerate(DataSet):
-    if index < 10:
+    if index < 120:
         position.append(None)
         continue
-    if item[2] > np.max(DataSet[index-9:index,2]):
-        if item[5] >= 100000:
-            if abs(item[4]/DataSet[index-1,4]) >= 1.01:
-                # print("third")
-                # print(item[4], DataSet[index-1,4])
-                if item[4] > item[1]:
-                    #print(short_values[index]/mid_values[index],short_values[index], mid_values[index], long_values[index])
-                    if (abs(short_values[index]/mid_values[index]) < 1.035) and (abs(short_values[index]/mid_values[index]) >= 0.965):
-                        if (abs(mid_values[index] / long_values[index]) < 1.03) and (abs(mid_values[index] / long_values[index]) >= 0.97):
-                            if (position[index-1] == 1) or (position[index-1] == 3):
-                                position.append(2)
-                            else:
-                                position.append(1)
-                        else:
-                            position.append(None)
-                    else:
-                        position.append(None)
-                else:
-                    position.append(None)
-            else:
-                position.append(None)
+    if (item[2] > np.max(DataSet[index-10:index,2])) and \
+            (item[5] >= 100000) and \
+            (abs(item[4]/DataSet[index-1,4]) >= 1.01) and \
+            (item[4] > item[1]) and \
+            (abs(short_values[index] / mid_values[index]) < 1.035) and (abs(short_values[index] / mid_values[index]) >= 0.965) and \
+            (abs(mid_values[index] / long_values[index]) < 1.03) and (abs(mid_values[index] / long_values[index]) >= 0.97):
+        if (position[index-1] == 1) or (position[index-1] == 3):
+            position.append(2)
+        else:
+            position.append(1)
+    else:
+        if (position[index-1] == 1) or (position[index-1] == 3):
+            position.append(2)
         else:
             position.append(None)
-    else:
-        position.append(None)
 print(position)
+
+for index, item in enumerate(DataSet):
+    if position[index] == 1:
+
+        866991600
